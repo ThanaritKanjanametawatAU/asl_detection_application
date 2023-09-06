645bc625ccf2bcd2e76e09873eb1a1ad9caee14f
ThanaritKanjanametawat <83484418+ThanaritKanjanametawat@users.noreply.github.com>
2023-09-06 15:10:36 +0700

    feat(lib): Make the history page scrollable

--- lib/history_page.dart
+++ lib/history_page.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 
 class HistoryPage extends StatelessWidget {
-  final List<File>? capturedImages;  // Notice the question mark, making it nullable
+  final List<File>? capturedImages;
 
   HistoryPage({this.capturedImages});
 
@@ -12,19 +12,21 @@ class HistoryPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("History"),
       ),
-      body: ListView.builder(
-        itemCount: capturedImages?.length ?? 0,  // Null-aware
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Image.file(
-              capturedImages![index],  // Using ! to assert that it's non-null at this point
-              height: 300,
-              width: 300,
-              fit: BoxFit.cover,
-            ),
-          );
-        },
+      body: Scrollbar(  // Added Scrollbar here
+        child: ListView.builder(
+          itemCount: capturedImages?.length ?? 0,  // Null-aware
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.file(
+                capturedImages![index],  // Using ! to assert that it's non-null at this point
+                height: 300,
+                width: 300,
+                fit: BoxFit.cover,
+              ),
+            );
+          },
+        ),
       ),
     );
   }
